02f3efcd77e3b5314a714fd1246feee6d3653880
github username <github email address>
2018-07-17 15:56:00 -0400

    added space

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,7 +4,7 @@ def welcome
 end
 
 def get_character_from_user
-  puts "Please enter a character:"
+  puts "Please enter a character: "
   character = gets.chomp
   character.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
