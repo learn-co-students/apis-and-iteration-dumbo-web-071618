c1ffabacfdbd80ff0bab42a17719c30549885ed3
personabe1984 <personabe1984@gmail.com>
2018-07-17 15:37:26 -0400

    testing

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,9 +6,27 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
+  #films_array = []
+
+  character_hash["results"].each do |character_attr|
+    if character_attr["name"] == character
+      character_films = character_attr["films"]
+      #puts character_films
+
+      character_films.each do |film|
+        specific_film_info = RestClient.get(film)
+        films_hash_info = JSON.parse(specific_film_info)
+        films_array << films_hash_info
+      end
+    end
+  end
+  #binding.pry
+  #puts
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
@@ -16,6 +34,11 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
+
+  parse_character_movies(films_array)
+
+  films_array
 end
 
 def parse_character_movies(films_hash)
@@ -31,3 +54,5 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+puts get_character_movies_from_api("Luke Skywalker")
