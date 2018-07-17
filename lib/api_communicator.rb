a88599b23f0949e5529971f6ba4f3df6bc0ffcec
personabe1984 <personabe1984@gmail.com>
2018-07-17 15:43:20 -0400

    testing

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -5,32 +5,6 @@ require 'pry'
 def get_character_info(character)
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-<<<<<<< HEAD
-
-
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  #films_array = []
-
-  character_hash["results"].each do |character_attr|
-    if character_attr["name"] == character
-      character_films = character_attr["films"]
-      #puts character_films
-
-      character_films.each do |film|
-        specific_film_info = RestClient.get(film)
-        films_hash_info = JSON.parse(specific_film_info)
-        films_array << films_hash_info
-      end
-    end
-  end
-  #binding.pry
-  #puts
-
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-=======
   character_hash
 end
 
@@ -49,20 +23,10 @@ def get_character_movies_from_api(character)
       character_data["films"]
   end
 end
->>>>>>> 3b4690d42e520e2813ab81c0ac53f53f763a3d6f
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
-<<<<<<< HEAD
-
-
-  parse_character_movies(films_array)
-
-  films_array
-end
-=======
->>>>>>> 3b4690d42e520e2813ab81c0ac53f53f763a3d6f
 
 def parse_character_movies(films_hash)
   # films_hash = array of urls
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,13 +1,10 @@
 def welcome
-  puts "Welcome to the most amazing app in a galaxy far, far away!"
-  puts " "
+  # puts out a welcome message here!
 end
 
 def get_character_from_user
-  puts "Please enter a character:"
-  character = gets.chomp
-  character.downcase
+  puts "please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
-  chracter = gets.chomp.downcased
-  
+  chracter = gets.chomp.downcase!
+
 end
